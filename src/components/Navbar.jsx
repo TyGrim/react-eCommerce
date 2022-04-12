de3fb2cd33263711ff4f4746c47a2fba8644ad67
TyGrim <tysongyorfy@gmail.com>
2022-04-11 19:12:33 -0700

    @mui installed icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styledComponents from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Container = styledComponents.div`
 height: 60px;
@@ -9,15 +10,32 @@ height: 60px;
 const Wrapper = styledComponents.div`
 display:flex;
 justify-content: space-between;
+align-items: center;
 padding: 10px 20px;
 `;
 const Left = styledComponents.div`
+display:flex;
+align-items: center;
 flex:1;
 `;
 
 const Language = styledComponents.span`
 font-size: 14px;
+
 cursor: pointer;
+`;
+
+const SearchBar = styledComponents.div`
+display: flex;
+align-items: center;
+border: 0.5px solid black;
+margin-left: 25px;
+padding: 5px;
+border: none;
+`;
+
+const Input = styledComponents.input`
+
 `;
 
 const Right = styledComponents.div`
@@ -33,6 +51,10 @@ function Navbar() {
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchBar>
+            <Input />
+            <SearchIcon />
+          </SearchBar>
         </Left>
         <Center>center</Center>
         <Right>right</Right>
