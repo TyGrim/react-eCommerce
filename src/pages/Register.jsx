3450ae291fa5e4e63226e08261ed0b86b36f512a
iThai <ithai@iThais-MacBook-Pro.local>
2022-04-26 16:31:08 -0700

    Register/signup page styled

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,79 @@
 import React from "react";
 import styled from "styled-components";
-import Alert from "../components/Alert";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(195, 110, 192, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1650783756107-739513b38177?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"),
+    no-repeat center center fixed;
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 50%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 50%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 50%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
 
 function Register() {
   return (
     <Container>
-      <Navbar />
-      <Alert />
-      <Footer />
+      <Wrapper>
+        <Title>CREATE YOUR ACCOUNT</Title>
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="last name" />
+          <Input placeholder="phone number" />
+          <Input placeholder="email" />
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an accont, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+        </Form>
+        <Button>SIGN UP</Button>
+      </Wrapper>
     </Container>
   );
 }
